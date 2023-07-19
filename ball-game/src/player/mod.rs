3a15c2c223092fe8a41e0ee02f7ea3efacd4dfbf
Igor <igorlfs@ufmg.br>
2023-07-19 17:39:57 -0300

    part8: system ordering

--- ball-game/src/player/mod.rs
+++ ball-game/src/player/mod.rs
@@ -1,18 +1,37 @@
-use bevy::prelude::*;
-
 use self::systems::*;
+use bevy::prelude::*;
 
 pub mod components;
 mod systems;
+
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct MovementSystemSet;
+//
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct ConfinementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub enum PlayerSystemSet {
+    Movement,
+    Confinement,
+}
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, player_movement)
-            .add_systems(Update, confine_player_movement)
-            .add_systems(Update, player_hit_stars)
-            .add_systems(Update, player_hit_enemies);
+        app.configure_set(
+            Update,
+            PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement),
+        )
+        .add_systems(Startup, spawn_player)
+        .add_systems(Update, player_movement.in_set(PlayerSystemSet::Movement))
+        .add_systems(
+            Update,
+            confine_player_movement.in_set(PlayerSystemSet::Confinement),
+        )
+        // .add_systems(Update, (player_movement, confine_player_movement).chain())
+        .add_systems(Update, player_hit_stars)
+        .add_systems(Update, player_hit_enemies);
     }
 }
